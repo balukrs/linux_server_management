cc23862aa773e52f06bcbfc04a708721d24a6b51
Balu Krishna R <balukrs@outlook.com>
2026-02-17 18:32:13 +0530

    pr updates added

--- apps/api/src/routes/v1/admin/accessCode/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-import { generateAccessCode } from './controller'
-
-const AccessCode: Router = Router()
-
-AccessCode.use(generateAccessCode)
-
-export default AccessCode
--- apps/api/src/routes/v1/admin/index.ts
+++ apps/api/src/routes/v1/admin/index.ts
@@ -1,12 +1,12 @@
 import adminRouteHandler from '#middleware/admin-route-handling.js'
 import { Router } from 'express'
 
-import AccessCode from './accessCode'
+import { generateAccessCode } from './accessCode/controller'
 import Users from './users'
 
 const Admin: Router = Router()
 
 Admin.get('/users', adminRouteHandler, Users)
-Admin.post('/generatecode', adminRouteHandler, AccessCode)
+Admin.post('/generatecode', adminRouteHandler, generateAccessCode)
 
 export default Admin
